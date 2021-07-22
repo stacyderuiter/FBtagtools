9f40cbdf5a9cf75142634bf1e70b4ab8a725bee4
Stacy DeRuiter <stacy.lynn.deruiter@gmail.com>
2021-07-22 08:19:20 -0400

    typo - paren closed in wrong spot

--- R/dive_acoustic_summary.R
+++ R/dive_acoustic_summary.R
@@ -620,7 +620,7 @@ dive_acoustic_summary <- function(tag_id = zc_smrt_tag_list,
     these_dives <- these_dives %>%
       dplyr::mutate(horiz_speed_km_h = distance_traveled_km / ((final_loc_time - initial_loc_time) / 3600))
     these_dives <- these_dives %>%
-      dplyr::mutate(horiz_speed_km_h = ifelse(is.infinite(horiz_speed_km_h, 0, horiz_speed_km_h)))
+      dplyr::mutate(horiz_speed_km_h = ifelse(is.infinite(horiz_speed_km_h), 0, horiz_speed_km_h))
 
     # Add bathy info
     if (!missing(bathy_path)){
