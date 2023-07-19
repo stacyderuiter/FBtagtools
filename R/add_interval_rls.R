28c80b9f065aed2c704f6101291f52e012262392
Stacy DeRuiter <stacy.lynn.deruiter@gmail.com>
2023-07-19 12:27:11 -0400

    change variable names to lower case in ping data and use new pipe

--- R/add_interval_rls.R
+++ R/add_interval_rls.R
@@ -8,41 +8,45 @@
 #' @param end_x name of variable in x (quotes not needed) with end times of intervals in seconds since start of tag recording
 #' @param start_ping name of variable in ping_data giving start times of pings in seconds since start of tag recording
 #' @return x with additional columns giving average, min, max, etc. RLs in each time interval
-#' @importFrom magrittr "%>%"
+#' @importFrom magrittr "|>"
 #' @export
 add_interval_rls <- function(x, ping_data, start_x, end_x, start_ping){
   start_x = rlang::enquo(start_x)
   end_x = rlang::enquo(end_x)
   start_ping = rlang::enquo(start_ping)
+
+  ping_data <- ping_data |>
+    dplyr::rename_with(tolower)
+
   # note: may want to future edit so that does SEL too? Need to get all energy and then sum
   x <- interval_join(x,
-                     ping_data %>% dplyr::select(sec_since_tagon,
+                     ping_data |> dplyr::select(sec_since_tagon,
                                                  duration,
-                                                 BB_RMS, Type) %>%
+                                                 bb_rms, type) |>
                        dplyr::rename(ping_duration = duration),
                      start_x = !!start_x,
                      end_x = !!end_x,
                      start_y = !!start_ping)
 
-  x <- x %>%
-    dplyr::group_by_all() %>% # includes groups by signal Type
-    dplyr::ungroup(sec_since_tagon, BB_RMS, ping_duration) %>%
+  x <- x |>
+    dplyr::group_by_all() |> # includes groups by signal type
+    dplyr::ungroup(sec_since_tagon, bb_rms, ping_duration) |>
     dplyr::summarise(
       n_pings = sum(!is.na(sec_since_tagon)),
       ping_dur_mean_sec = mean(ping_duration, na.rm = TRUE),
       ping_dur_min_sec = min(ping_duration, na.rm = TRUE),
       ping_dur_max_sec = max(ping_duration, na.rm = TRUE),
-      bb_rms_min = min(BB_RMS, na.rm = TRUE),
-      bb_rms_max = max(BB_RMS, na.rm = TRUE),
-      bb_rms_median = median(BB_RMS, na.rm = TRUE),
-      bb_rms_mean = suppressWarnings(10 * log10(mean(10 ^ (na.omit(BB_RMS) / 10)))),
+      bb_rms_min = min(bb_rms, na.rm = TRUE),
+      bb_rms_max = max(bb_rms, na.rm = TRUE),
+      bb_rms_median = median(bb_rms, na.rm = TRUE),
+      bb_rms_mean = suppressWarnings(10 * log10(mean(10 ^ (na.omit(bb_rms) / 10)))),
       bb_rms_mean = ifelse(is.infinite(bb_rms_mean) |
                              is.na(bb_rms_mean),
                            NA,
-                           bb_rms_mean))   %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(Type = tolower(Type)) %>%
-    tidyr::pivot_wider(names_from = Type,
+                           bb_rms_mean))   |>
+    dplyr::ungroup() |>
+    dplyr::mutate(type = tolower(type)) |>
+    tidyr::pivot_wider(names_from = type,
                        values_from = c(n_pings,
                                        ping_dur_mean_sec,
                                        ping_dur_min_sec,
@@ -51,11 +55,11 @@ add_interval_rls <- function(x, ping_data, start_x, end_x, start_ping){
                                        bb_rms_max,
                                        bb_rms_median,
                                        bb_rms_mean),
-                       names_glue = "{Type}_{.value}" # put the mfa_ echo_ etc. FIRST not last
-    ) %>%
+                       names_glue = "{type}_{.value}" # put the mfa_ echo_ etc. FIRST not last
+    ) |>
     # if there are no non-missing RLs then some of the results will be Inf instead of missing
     # but they should just be missing
-    dplyr::select(!tidyselect::starts_with('NA_')) %>%
+    dplyr::select(!tidyselect::starts_with('NA_')) |>
     dplyr::mutate(dplyr::across(contains('mfa') | contains('echosounder') | contains('explos'),
                                 ~ifelse(is.infinite(.x), NA, .x)))
 
