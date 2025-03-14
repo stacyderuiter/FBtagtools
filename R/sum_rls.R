bc0fc7a4bac1dce3abf8cfbbc2b99787117895f2
Stacy DeRuiter <stacy.lynn.deruiter@gmail.com>
2025-03-14 16:54:20 -0400

    update sum rls

--- R/sum_rls.R
+++ R/sum_rls.R
@@ -3,18 +3,11 @@
 #' Add up RLs in several frequency bands. Used by \code{\link[FBtagtools]{extract_rls}}.
 #'
 #' @param rls numeric vector with RLs in dB
-#' @param energy Whether or not the RLs are in energy units (alternative is intensity)
 #' @return sum of the received levels in `rls`
 #' @export
 #' @examples
 #' # coming soon
-sum_rls <- function(rls,
-                    energy = FALSE){
-  if (!energy){
+sum_rls <- function(rls){
     rls <- stats::na.omit(rls)
     10 * log10( sum(10 ^ (rls / 10)))
-  }else{
-    rls <- stats::na.omit(rls)
-    20 * log10( sum(10 ^ (rls / 20)))
-  }
 }
